bfddf2c7ae3a9d590f89cf7f690b4bff2de32edf
Tiger <thisisstreamliner@gmail.com>
2022-04-23 14:19:45 +0300

    Add VerticalIconButton

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/widgets/vertical_icon_button.dart';
 
 import '../data/content_model.dart';
 
@@ -41,13 +42,37 @@ class ContentHeader extends StatelessWidget {
             child: Image.asset(featuredContent.titleImageUrl!),
           ),
         ),
-        const Positioned(
+        Positioned(
           left: 0.0,
           right: 0.0,
           bottom: 40.0,
-          child: SizedBox.shrink(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              VerticalIconButton(icon: Icons.add, title: 'List', onTap: () => print('my list')),
+              _PlayButton(),
+              VerticalIconButton(icon: Icons.info_outline, title: 'Info', onTap: () => print('info'))
+            ],
+          ),
         ),
       ],
     );
   }
 }
+
+class _PlayButton extends StatelessWidget {
+  const _PlayButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(15.0, 5.0, 20.0, 5.0),
+      child: TextButton.icon(
+        icon: Icon(Icons.play_arrow, size: 30.0,),
+        onPressed: () => print('play'),
+        label: const Text('Play', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),),
+      ),
+    );
+  }
+}
+
